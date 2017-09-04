6492f878d2e5cdca4879f2e6b885c9639fa919ef
Dabbikarls <dabbikarls@gmail.com>
2017-09-04 12:33:14 +0000

    Add files via upload

---
+++ verkefni_4.py
@@ -0,0 +1,16 @@
+#Davíð Freyr Karlsson
+#04/09/2017
+
+tala1 = int(input("Sláðu inn eina heiltölu"))
+tala2 = int(input("Sláðu inn aðra heiltölu"))
+if tala1 < tala2:
+    print(tala1, "er minni en",tala2)
+elif tala2 < tala1:
+    print(tala2,"er minni en",tala1)
+elif tala1 == tala2:
+    print("Þær eru jafn stórar.")
+
+#Lidur 2
+
+manudur = input("Hvaða mánuður er í dag? ")
+if manudur == "Januar"
